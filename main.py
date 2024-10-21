0a21320e6f6b03dedecd68d481a1b3ef5f9dd13c
pupya <149272630+pupyadev@users.noreply.github.com>
2024-10-21 18:55:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,108 @@ def coin_colliderect():
     coin_exists = False
     del coin_rect
 
+def spike_colliderect():
+    global spike_exists, spike_rect, player_kill, explosion_rect, current_frame_explosion
+    print('Kill!')
+    spike_exists = False
+    del spike_rect
+    player_kill = True
+    current_frame_explosion = 0
+    explosion_rect.center = player_rect.center
+
+# while True:
+#     window.fill(BACKGROUND_COLOR)
+#     window.blit(floor, floor_rect)
+#
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+#
+#     keys = pygame.key.get_pressed()
+#
+#     if coin_exists and player_rect.colliderect(coin_rect):
+#         coin_colliderect()
+#
+#     if coin_exists:
+#         current_frame_coin = (current_frame_coin + 1) % 8
+#         window.blit(coin[current_frame_coin], coin_rect)
+#
+#     if spike_exists and player_rect.colliderect(spike_rect):
+#         spike_colliderect()
+#
+#     if spike_exists:
+#         window.blit(spike, spike_rect)
+#
+#     if not player_kill:
+#         if keys[pygame.K_RIGHT]:
+#             if not floor_rect.left <= -520:
+#                 floor_rect.centerx -= player_speed
+#                 if coin_exists:
+#                     coin_rect.centerx -= player_speed
+#                 if spike_exists:
+#                     spike_rect.centerx -= player_speed
+#             if not player_kill:
+#                 current_frame_right = (current_frame_right + 1) % 4
+#                 window.blit(player_right[current_frame_right], player_rect)
+#         elif keys[pygame.K_LEFT]:
+#             if not floor_rect.left >= 125:
+#                 floor_rect.centerx += player_speed
+#                 if coin_exists:
+#                     coin_rect.centerx += player_speed
+#                 if spike_exists:
+#                     spike_rect.centerx += player_speed
+#             if not player_kill:
+#                 current_frame_left = (current_frame_left + 1) % 4
+#                 window.blit(player_left[current_frame_left], player_rect)
+#         elif keys[pygame.K_UP]:
+#             if not floor_rect.top >= 40:
+#                 floor_rect.centery += player_speed
+#                 if coin_exists:
+#                     coin_rect.centery += player_speed
+#                 if spike_exists:
+#                     spike_rect.centery += player_speed
+#             else:
+#                 if not player_rect.top <= 60:
+#                     player_rect.centery -= player_speed
+#             if not player_kill:
+#                 current_frame_up = (current_frame_up + 1) % 4
+#                 window.blit(player_up[current_frame_up], player_rect)
+#         elif keys[pygame.K_DOWN]:
+#             if not floor_rect.top <= -200:
+#                 floor_rect.centery -= player_speed
+#                 if coin_exists:
+#                     coin_rect.centery -= player_speed
+#                 if spike_exists:
+#                     spike_rect.centery -= player_speed
+#             else:
+#                 if not player_rect.top >= 200:
+#                     player_rect.centery += player_speed
+#             if not player_kill:
+#                 current_frame_down = (current_frame_down + 1) % 4
+#                 window.blit(player_down[current_frame_down], player_rect)
+#         else:
+#             window.blit(player, player_rect)
+#     else:
+#         # Анимация взрыва
+#         if current_frame_explosion < len(explosion):
+#             window.blit(explosion[current_frame_explosion], explosion_rect)
+#             current_frame_explosion += 1
+#         else:
+#             # Действие после окончания анимации взрыва
+#             pygame.quit()
+#             sys.exit()
+#
+#     # Обновление текста и интерфейса
+#     text_rect = font2.render(f'x={floor_rect.centerx} y={floor_rect.centery}', True, (255, 255, 255))
+#     window.blit(line, line_rect)
+#     window.blit(text_score, text_score_rect)
+#     window.blit(line2, line2_rect)
+#     window.blit(text_rect, text_rect_rect)
+#
+#     clock.tick(FPS)
+#     pygame.display.update()
+
 while True:
     window.fill(BACKGROUND_COLOR)
     window.blit(floor, floor_rect)
@@ -32,42 +134,72 @@ while True:
         current_frame_coin = (current_frame_coin + 1) % 8
         window.blit(coin[current_frame_coin], coin_rect)
 
-    if keys[pygame.K_RIGHT]:
-        if not floor_rect.left <= -520:
-            floor_rect.centerx -= player_speed
-            if coin_exists:
-                coin_rect.centerx -= player_speed
-        current_frame_right = (current_frame_right + 1) % 4
-        window.blit(player_right[current_frame_right], player_rect)
-    elif keys[pygame.K_LEFT]:
-        if not floor_rect.left >= 125:
-            floor_rect.centerx += player_speed
-            if coin_exists:
-                coin_rect.centerx += player_speed
-        current_frame_left = (current_frame_left + 1) % 4
-        window.blit(player_left[current_frame_left], player_rect)
-    elif keys[pygame.K_UP]:
-        if not floor_rect.top >= 40:
-            floor_rect.centery += player_speed
-            if coin_exists:
-                coin_rect.centery += player_speed
+    if spike_exists:
+        if player_rect.colliderect(spike_rect):
+            spike_colliderect()
+
+    if spike_exists:
+        window.blit(spike, spike_rect)
+
+    if pause_open:
+        window.blit(menu, menu_rect)
+        window.blit(restart, restart_rect)
+        window.blit(play, play_rect)
+
+    if not pause_open:
+        if keys[pygame.K_RIGHT]:
+            if not floor_rect.left <= -520:
+                floor_rect.centerx -= player_speed
+                if coin_exists:
+                    coin_rect.centerx -= player_speed
+                if spike_exists:
+                    spike_rect.centerx -= player_speed
+            if not player_kill:
+                current_frame_right = (current_frame_right + 1) % 4
+                window.blit(player_right[current_frame_right], player_rect)
+        elif keys[pygame.K_LEFT]:
+            if not floor_rect.left >= 125:
+                floor_rect.centerx += player_speed
+                if coin_exists:
+                    coin_rect.centerx += player_speed
+                if spike_exists:
+                    spike_rect.centerx += player_speed
+            if not player_kill:
+                current_frame_left = (current_frame_left + 1) % 4
+                window.blit(player_left[current_frame_left], player_rect)
+        elif keys[pygame.K_UP]:
+            if not floor_rect.top >= 40:
+                floor_rect.centery += player_speed
+                if coin_exists:
+                    coin_rect.centery += player_speed
+                if spike_exists:
+                    spike_rect.centery += player_speed
+            else:
+                if not player_rect.top <= 60:
+                    player_rect.centery -= player_speed
+            if not player_kill:
+                current_frame_up = (current_frame_up + 1) % 4
+                window.blit(player_up[current_frame_up], player_rect)
+        elif keys[pygame.K_DOWN]:
+            if not floor_rect.top <= -200:
+                floor_rect.centery -= player_speed
+                if coin_exists:
+                    coin_rect.centery -= player_speed
+                if spike_exists:
+                    spike_rect.centery -= player_speed
+            else:
+                if not player_rect.top >= 200:
+                    player_rect.centery += player_speed
+            if not player_kill:
+                current_frame_down = (current_frame_down + 1) % 4
+                window.blit(player_down[current_frame_down], player_rect)
         else:
-            if not player_rect.top <= 60:
-                player_rect.centery -= player_speed
-        current_frame_up = (current_frame_up + 1) % 4
-        window.blit(player_up[current_frame_up], player_rect)
-    elif keys[pygame.K_DOWN]:
-        if not floor_rect.top <= -200:
-            floor_rect.centery -= player_speed
-            if coin_exists:
-                coin_rect.centery -= player_speed
-        else:
-            if not player_rect.top >= 200:
-                player_rect.centery += player_speed
-        current_frame_down = (current_frame_down + 1) % 4
-        window.blit(player_down[current_frame_down], player_rect)
-    else:
-        window.blit(player, player_rect)
+            if not player_kill:
+                window.blit(player, player_rect)
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    if pause_rect.left < mouse_x < pause_rect.right and pause_rect.top < mouse_y < pause_rect.bottom:
+        print('Ok')
 
     # Text
     text_rect = font2.render(f'x={floor_rect.centerx} y={floor_rect.centery}', True, (255, 255, 255))
@@ -75,8 +207,11 @@ while True:
     # Render
     window.blit(line, line_rect)
     window.blit(text_score, text_score_rect)
+    window.blit(pause, pause_rect)
     window.blit(line2, line2_rect)
     window.blit(text_rect, text_rect_rect)
+
+    #window.blit(explosion[current_frame_explosion], explosion_rect)
 
     clock.tick(FPS)
     pygame.display.update()
--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ score = 0
 floor = pygame.image.load('src/world/floor.png')
 floor_rect = floor.get_rect()
 floor_rect.centerx = WINDOW_WIDTH // 2
-floor_rect.centery = 295
+floor_rect.centery = 215
 
 # Player
 player = pygame.image.load('src/player/3.png')
@@ -58,6 +58,7 @@ current_frame_left = 0
 current_frame_right = 0
 
 player_speed = 5
+player_kill = False
 
 # Coin
 coin = []
@@ -75,9 +76,44 @@ coin_rect.centery = random.randint(60, 280)
 
 coin_exists = True
 
+# Spike
+spike = pygame.image.load('src/spike/spike.png')
+spike = pygame.transform.scale(spike, (35, 35))
+spike_rect = spike.get_rect()
+spike_rect.centerx = WINDOW_WIDTH // 2
+spike_rect.centery = WINDOW_WIDTH // 2 + 40
+
+spike_exists = True
+
+# Explosion
+#explosion = []
+
+#for frame in range(4, 7):
+#    image = pygame.image.load(f'src/explosion/{frame}.png')
+#    image = image.convert_alpha()
+#    image = pygame.transform.scale(image, (40, 40))
+#    #explosion.append(image)
+
+#current_frame_explosion = 0
+#explosion_rect = explosion[current_frame_explosion].get_rect()
+#explosion_rect.centerx = WINDOW_WIDTH // 2
+#explosion_rect.centery = WINDOW_WIDTH // 2
+
+explosion = []
+
+for frame in range(0, 4):
+    image = pygame.image.load(f'src/explosion/{frame}.png')
+    image = image.convert_alpha()
+    image = pygame.transform.scale(image, (40, 40))
+    explosion.append(image)
+
+current_frame_explosion = 0
+explosion_rect = explosion[current_frame_explosion].get_rect()
+explosion_rect.centerx = WINDOW_WIDTH // 2
+explosion_rect.centery = WINDOW_WIDTH // 2 + 40
+
 # Sound
 sound_coin = pygame.mixer.Sound('src/music/coin.mp3')
-
 pygame.mixer.music.load('src/music/music.mp3')
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
@@ -101,11 +137,36 @@ line_rect = line.get_rect()
 line_rect.centerx = WINDOW_WIDTH // 2
 line_rect.centery = 20
 
+pause = pygame.image.load('src/ui/pause.png')
+pause_rect = pause.get_rect()
+pause_rect.centerx = 375
+pause_rect.centery = 20
+
+pause_open = False
+
 line2 = pygame.image.load('src/ui/line2.png')
 line2_rect = line2.get_rect()
 line2_rect.centerx = WINDOW_WIDTH // 2
 line2_rect.centery = WINDOW_HEIGHT
 
+menu = pygame.image.load('src/ui/menu.png')
+menu = pygame.transform.scale(menu, (270, 225))
+menu_rect = menu.get_rect()
+menu_rect.centerx = WINDOW_WIDTH // 2
+menu_rect.centery = WINDOW_WIDTH // 2 - 20
+
+restart = pygame.image.load('src/ui/restart.png')
+restart = pygame.transform.scale(restart, (100, 100))
+restart_rect = restart.get_rect()
+restart_rect.centerx = WINDOW_WIDTH // 2 - 60
+restart_rect.centery = WINDOW_WIDTH // 2 - 20
+
+play = pygame.image.load('src/ui/play.png')
+play = pygame.transform.scale(play, (120, 120))
+play_rect = restart.get_rect()
+play_rect.centerx = WINDOW_WIDTH // 2 + 60
+play_rect.centery = WINDOW_WIDTH // 2 - 30
+
 # Render
 clock = pygame.time.Clock()
-FPS = 15
+FPS = 20
